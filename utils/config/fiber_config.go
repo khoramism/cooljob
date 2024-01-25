60ff20d60aedcc368211f52aa9679cbcc509ee14
khoramism <khoramism@gmail.com>
2024-01-25 08:49:04 +0000

    adding middlewares'

--- utils/config/fiber_config.go
+++ utils/config/fiber_config.go
@@ -1,9 +1,14 @@
 package configs
 
 import (
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 // FiberConfig func for configuration Fiber app.
@@ -19,3 +24,60 @@ func FiberConfig() fiber.Config {
 		AppName:       "CoolJob",
 	}
 }
+
+func CompressionConfig() compress.Config {
+	return compress.Config{
+		Next:  nil,
+		Level: compress.LevelBestCompression,
+	}
+}
+
+func CorsConfig() cors.Config {
+	return cors.Config{
+		AllowOrigins: "https://cooljob.ir",
+		AllowHeaders: "Origin, Content-Type, Accept",
+
+		// We don't need more methods yet
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodHead,
+		}, ","),
+	}
+}
+
+func LoggerConfig() logger.Config {
+	return logger.Config{
+		Format:     "[${ip}]:${port} ${status} - ${method} ${path}\n",
+		TimeFormat: "2006-01-02",
+		TimeZone:   "Asia/Tehran",
+	}
+}
+
+func LimiterConfig() limiter.Config {
+	return limiter.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.IP() == "https://cooljob.ir"
+		},
+		Max:        20,
+		Expiration: 30 * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.Get("x-forwarded-for")
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Redirect("https://www.google.com/search?&q=bad+boy")
+		},
+	}
+}
+
+// func RedisConfig() redis.Config {
+// 	return redis.Config{
+// 		Host:      "redis-db",
+// 		Port:      6379,
+// 		Username:  "",
+// 		Password:  "",
+// 		Database:  0,
+// 		Reset:     false,
+// 		TLSConfig: nil,
+// 		PoolSize:  10 * runtime.GOMAXPROCS(0),
+// 	}
+// }
--- utils/middleware/fiber_middleware.go
+++ utils/middleware/fiber_middleware.go
@@ -2,16 +2,21 @@ package middleware
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	configs "github.com/khoramism/cooljob/utils/config"
 )
 
 // FiberMiddleware provide Fiber's built-in middlewares.
 // See: https://docs.gofiber.io/api/middleware
 func FiberMiddleware(a *fiber.App) {
 	a.Use(
-		cors.New(),
+		cors.New(configs.CorsConfig()),
 		// Add simple logger.
-		logger.New(),
+		logger.New(configs.LoggerConfig()),
+		compress.New(configs.CompressionConfig()),
+		limiter.New(configs.LimiterConfig()),
 	)
 }
